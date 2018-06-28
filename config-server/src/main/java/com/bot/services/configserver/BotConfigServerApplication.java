ddbb0dda5f6a6d538be73733085a3e992497e14c
Kishore Nallagatla <kishore2310@gmail.com>
2018-06-27 23:36:46 -0400

    Class name change for config server's main class

--- config-server/src/main/java/com/bot/services/configserver/BotConfigServerApplication.java
+++ config-server/src/main/java/com/bot/services/configserver/BotConfigServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class BotConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(BotConfigServerApplication.class, args);
 	}
 }
